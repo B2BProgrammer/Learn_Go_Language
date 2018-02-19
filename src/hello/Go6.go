161f200f7089719c62e2eb60de246ebbd38d99d0
ajithajjarani <ajith.jnnce06@gmail.com>
2018-02-19 09:11:34 -0800

    commit_3

---
+++ src/hello/Go6.go
@@ -0,0 +1,30 @@
+package main
+
+import 	"fmt"
+
+
+// Equivalent to class/Declartion of car class
+type car struct {
+	gas_pedal uint16 //min vlau = 0, max - 65535
+	brake_pedal uint16
+	steering_wheel int16 // -32k to +32k
+	top_speed_kmh float64
+}
+
+
+func main()  {
+
+	// Equivalent to object instatiate : creation of a_car_1 object
+	a_car_1 := car{ gas_pedal: 22341,
+				brake_pedal: 0,
+				steering_wheel: 12561,
+				top_speed_kmh: 225.0}
+
+	// Equivalent to object instatiate : creation of a_car_2 object
+	a_car_2 :=car{22343,2,233, 100}
+
+
+	fmt.Println(a_car_1.gas_pedal)
+	fmt.Println(a_car_2.top_speed_kmh)
+}
+		
