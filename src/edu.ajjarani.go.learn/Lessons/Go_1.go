f0c10dce6d2c237c00191b1c17c3449e2b39d4d4
ajithajjarani <ajith.jnnce06@gmail.com>
2018-04-20 12:22:15 -0700

    u

--- src/edu.ajjarani.go.learn/Lessons/Go_1.go
+++ src/edu.ajjarani.go.learn/Lessons/Go_1.go
@@ -6,6 +6,7 @@ Lesson 1 :  A simple lesson to say "Hello World from Go !!"
 
 1. fmt is the basic library to be imported
 2. Package main & func main() is the start point of the runtime
+
  */
 import "fmt"
 
